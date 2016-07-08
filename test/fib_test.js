2f4bd42560d040bdde7924b53d61b9b4a4b3bb2a
Alyson Takai <shtakai@gmail.com>
2016-07-07 17:38:34 -0700

    modified

--- test/fib_test.js
+++ test/fib_test.js
@@ -4,48 +4,18 @@ var expect = require('chai').expect;
 
 var fib = require('../lib/fib');
 
-describe('filter', function(){
-  var fibCounter;
+describe('fib', function(){
 
-  before(function(){
-    fibCounter = fib();
-  });
-
-  function nTimesFib(n) {
-    console.log('call');
-    _(n).times(function(){fibCounter();});
-  }
-
-  it('1st call return 1', function(){
-    expect(fibCounter()).to.be.equal(1);
-  });
-
-  it('2st call return 1', function(){
-    nTimesFib(1);
-    expect(fibCounter()).to.be.equal(1);
-  });
-
-
-  it('3rd call return 2', function(){
-    nTimesFib(2);
-    expect(fibCounter()).to.be.equal(2);
-  });
-
-
-  it('4th call return 3', function(){
-    nTimesFib(3);
-    expect(fibCounter()).to.be.equal(3);
-  });
-
-
-  it('5th call return 5', function(){
-    nTimesFib(4);
-    expect(fibCounter()).to.be.equal(5);
-  });
-
-  it('6th call return 8', function(){
-    nTimesFib(5);
-    expect(fibCounter()).to.be.equal(8);
+  it('1..9 returns 1 1 2 3 5 8 13 21 34', function(){
+    expect(fib.nacci()).to.be.equal(1);
+    expect(fib.nacci()).to.be.equal(1);
+    expect(fib.nacci()).to.be.equal(2);
+    expect(fib.nacci()).to.be.equal(3);
+    expect(fib.nacci()).to.be.equal(5);
+    expect(fib.nacci()).to.be.equal(8);
+    expect(fib.nacci()).to.be.equal(13);
+    expect(fib.nacci()).to.be.equal(21);
+    expect(fib.nacci()).to.be.equal(34);
   });
 
 });
